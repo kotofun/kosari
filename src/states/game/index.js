fa5a9e339738eb9c06febc5a7571fb16c203fd90
Дмитрий Басавин <basavind@gmail.com>
2017-09-16 15:13:01 +0300

    🎨  rename Land class with Surface

--- src/states/game/index.js
+++ src/states/game/index.js
@@ -2,53 +2,54 @@
 import Phaser from 'phaser'
 import Player from '../../sprites/Player'
 
-import Land from './Land'
+import Surface from './Surface'
 import Background from './Background'
 import Controller from './Controller'
 
 export default class extends Phaser.State {
   init () {
-    this.background = new Background(this)
+    this.Background = new Background(this)
 
     this.distance = 0
 
-    this.player = new Player({ game: this.game, x: 240, y: 100, asset: 'player' })
+    this.Player = new Player({ game: this.game, x: 240, y: 100, asset: 'player' })
 
-    this.land = new Land(this)
+    this.Surface = new Surface(this)
   }
 
   preload () {
     this.game.physics.startSystem(Phaser.Physics.ARCADE)
     this.game.physics.arcade.gravity.y = 2000
 
-    this.game.physics.arcade.enable(this.player)
-    this.game.physics.arcade.enable(this.land)
+    this.game.physics.arcade.enable(this.Player)
+    this.game.physics.arcade.enable(this.Surface)
 
-    this.player.body.collideWorldBounds = true
-    this.game.add.existing(this.player)
+    this.game.add.existing(this.Player)
   }
 
   create () {
-    this.controller = new Controller(this)
+    this.Controller = new Controller(this)
 
-    this.background.startAnimation()
+    this.Player.body.collideWorldBounds = true
+
+    this.Background.startAnimation()
   }
 
   update () {
-    this.land.collide(this.player)
-    this.land.update()
+    this.Surface.collide(this.Player)
+    this.Surface.update()
 
     if (this.isGameOver()) {
       this.gameOver()
     }
 
-    if (this.controller.isJumped()) {
-      this.player.body.velocity.y = -600
+    if (this.Controller.isJumped()) {
+      this.Player.body.velocity.y = -600
     }
   }
 
   isGameOver () {
-    return this.player.right < this.game.world.bounds.left
+    return this.Player.right < this.game.world.bounds.left
   }
 
   gameOver () {
