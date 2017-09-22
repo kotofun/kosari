bd28316dbd2adf710925c89c28cfd1f2c6a33058
Дмитрий Басавин <basavind@gmail.com>
2017-09-22 13:36:02 +0300

    🚧  extend grass class from surface

--- src/sprites/Grass.js
+++ src/sprites/Grass.js
@@ -1,16 +1,7 @@
-import Phaser from 'phaser'
+import Surface from './Surface'
 
-export default class extends Phaser.Sprite {
+export default class extends Surface {
   constructor ({ game, x, y, speed = 0 }) {
     super(game, x, y, 'surface.grass', game.rnd.between(1, 2))
-
-    this.game.physics.arcade.enable(this)
-    this.body.allowGravity = false
-    this.body.immovable = true
-    this.speed = speed
-  }
-
-  set speed (value) {
-    this.body.velocity.x = value
   }
 }
